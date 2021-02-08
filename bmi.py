30ca36eda238a4f0a39a8e20ce4caa56b8cae6fd
Stephen Caulfield <g00398240@gmit.ie>
2021-02-08 22:02:51 +0000

    task fixes

--- bmi.py
+++ bmi.py
@@ -1,15 +1,20 @@
 #This program calculates the user's BMI using their inputs
 #author: Stephen Caulfield
 
+#Creating function to properly round numbers: credit: https://realpython.com/python-rounding/#rounding-half-up
+import math
+def round_half_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
 #inputs for height and weight
-weight = float (input ("enter weight: "))
-height = float (input ("enter height: "))
+weight = float (input ("enter weight(KG): "))
+height = float (input ("enter height(CM): "))
 
 #conversion of centimetres to metre squared
-metresquared = (height/100)**2
+metreSquared = (height/100)**2
 
 #calculation of BMI
-BMI = round(weight / metresquared, 2)
+BMI = round_half_up(weight / metreSquared, 2)
 
 #BMI output
 print("BMI is {}".format(BMI))
--- reversesecstring.py
+++ reversesecstring.py
@@ -1,5 +1,17 @@
+#This program takes a string input and prints every second letter backwards
+#author: Stephen Caulfield
+
+#user input for string
 inString = input("please enter a string:")
+
+#takes length of string as an integer
 i = len(inString)
+
+#removes every second letter from string starting starting with first letter(in "the"; "t" is removed)
 secLetter = inString[1:i:2]
+
+#creates the processed string backwards
 backString = secLetter[::-1]
+
+#prints final output to user
 print(backString)
